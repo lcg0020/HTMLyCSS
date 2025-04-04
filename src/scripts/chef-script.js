e137911a276884c352fe56da0647398a231bdb71
laucg00 <laucg00@hotmail.com>
2025-04-04 10:33:17 +0200

    Modifico rutas imágenes scripts 2

--- src/scripts/chef-script.js
+++ src/scripts/chef-script.js
@@ -4,8 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const btnNextChef  = document.getElementById("btn-next-chef");
 
     const images = [
-        '../../img/chef1.jpg',
-        '../../img/chef2.jpg'
+        '../img/chef1.jpg',
+        '../img/chef2.jpg'
     ];
 
     let currentIndex = 0;
