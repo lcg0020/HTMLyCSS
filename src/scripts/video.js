45072426dc8faa27eb1739ee450d0d7e131a6e0f
laucg00 <laucg00@hotmail.com>
2025-06-06 09:24:51 +0200

    Aplico mejoras Google

---
+++ src/scripts/video.js
@@ -0,0 +1,14 @@
+document.querySelectorAll(".video-thumbnail").forEach(thumbnail => {
+    thumbnail.addEventListener("click", function () {
+        let videoId = this.getAttribute("data-video");
+        let iframe = document.createElement("iframe");
+        iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+        iframe.width = "560";
+        iframe.height = "315";
+        iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
+        iframe.referrerPolicy = "strict-origin-when-cross-origin";
+        iframe.allowFullscreen = true;
+
+        this.parentNode.replaceChild(iframe, this);
+    });
+});
